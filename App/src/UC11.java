c1119709ac9cb950ca6acc58884646b0b0675cda
Dhee-S <ds8400@srmist.edu.in>
2026-03-15 10:33:28 +0530

    uc6 updated

---
+++ App/src/UC11.java
@@ -0,0 +1,41 @@
+
+public class UC11 {
+
+    private String word;
+
+    // Constructor
+    public UC11(String word) {
+        this.word = word;
+    }
+
+    // Public method to check palindrome
+    public boolean checkPalindrome() {
+
+        // Using two-pointer logic
+        int start = 0;
+        int end = word.length() - 1;
+
+        while (start < end) {
+            if (word.charAt(start) != word.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+
+    // Main method to demonstrate usage
+    public static void main(String[] args) {
+
+        UC11 checker = new UC11("madam");
+
+        if (checker.checkPalindrome()) {
+            System.out.println(checker.word + " is a Palindrome.");
+        } else {
+            System.out.println(checker.word + " is NOT a Palindrome.");
+        }
+    }
+}
+}
