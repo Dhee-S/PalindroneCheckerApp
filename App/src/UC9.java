68de50e53cceb76d7a9ea88bb2743c422a5cc6c6
Dhee-S <ds8400@srmist.edu.in>
2026-03-15 10:44:32 +0530

    UC9 updated

--- App/src/UC9.java
+++ App/src/UC9.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
 
 public class UC9 {
+
+    public static boolean isPalindrome(String word, int start, int end) {
+
+        // Base Condition
+        if (start >= end) {
+            return true;
+        }
+
+        // If mismatch found
+        if (word.charAt(start) != word.charAt(end)) {
+            return false;
+        }
+
+        // Recursive Call
+        return isPalindrome(word, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+
+        String word = "madam";
+
+        boolean result = isPalindrome(word, 0, word.length() - 1);
+
+        if (result) {
+            System.out.println(word + " is a Palindrome.");
+        } else {
+            System.out.println(word + " is NOT a Palindrome.");
+        }
+    }
+
 }
